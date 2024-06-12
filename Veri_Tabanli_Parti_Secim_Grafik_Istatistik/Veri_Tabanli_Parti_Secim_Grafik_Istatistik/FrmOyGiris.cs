e1930d6ae756a85ceeb33c538ce5b3249430ed34
berkiskitoglu <167026233+berkiskitoglu@users.noreply.github.com>
2024-06-12 14:02:58 +0300

    Secim Istatistik Ve Grafik Sistemi
    Seçim İstatistik ve Grafik Sistemi (SEÇSİS), Türkiye'deki seçim süreçlerinin yönetimini ve sonuçların sayımını elektronik olarak gerçekleştirmek için kullanılan bir sistemdir. Türkiye Cumhuriyeti Yüksek Seçim Kurulu (YSK) tarafından geliştirilen SEÇSİS, seçim güvenliği, şeffaflık ve hızını artırmayı amaçlamaktadır.

---
+++ Veri_Tabanli_Parti_Secim_Grafik_Istatistik/Veri_Tabanli_Parti_Secim_Grafik_Istatistik/FrmOyGiris.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Veri_Tabanli_Parti_Secim_Grafik_Istatistik
+{
+    public partial class FrmOyGiris : Form
+    {
+        public FrmOyGiris()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection baglanti = new SqlConnection(@"Data Source=berk;Initial Catalog=DBSECIMPROJE;Integrated Security=True;");
+        private void button1_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            SqlCommand komut = new SqlCommand("iNsert into TBLILCE(ILCEAD,APARTI,BPARTI,CPARTI,DPARTI,EPARTI) values (@p1,@p2,@p3,@p4,@p5,@p6)", baglanti);
+            komut.Parameters.AddWithValue("@p1", TxtIlce.Text);
+            komut.Parameters.AddWithValue("@p2", TxtA.Text);
+            komut.Parameters.AddWithValue("@p3", TxtB.Text);
+            komut.Parameters.AddWithValue("@p4", TxtC.Text);
+            komut.Parameters.AddWithValue("@p5", TxtD.Text);
+            komut.Parameters.AddWithValue("@p6", TxtE.Text);
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            MessageBox.Show("Oy Girişi Gerçekleşti");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            FrmGrafikler fr = new FrmGrafikler();
+            fr.Show();
+        }
+    }
+}
